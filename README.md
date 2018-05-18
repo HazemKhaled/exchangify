05759d54f27a6e2b44fa4472f75884cb44f00126
Pavle Andric <pavle.andric.zg@gmail.com>
2018-05-18 13:04:53 +0200

    Commit committed

---
+++ README.md
@@ -0,0 +1,62 @@
+
+# Exchangify
+
+<br/>
+<img src="https://res.cloudinary.com/disqnsnwa/image/upload/v1526634764/exchangify_logo.png" alt="My cool logo" width="300" height="300" />
+<br/>
+
+Simple, **100% free** and tiny JavaScript library for realtime currency conversion and exchange rate calculation, from any currency, to any currency.
+
+**exchangify** is integrated with [OpenExchangeRatesAPI](https://openexchangerates.org). 
+
+### Prerequisites
+First you need to obtain ```app_id``` from [OpenExchangeRatesAPI](https://openexchangerates.org). Then just proceed to the next section.
+
+### Install
+
+```
+npm i exchangify --save
+```
+
+### Example
+
+```
+const Exchangify = require("exchangify")
+const exchangify = new Exchangify("openexchangerates_app_id")
+```
+Using promises:
+```
+exchangify.exchange(100, "EUR", "HRK")
+    .then(amount => console.log(amount))
+```
+
+Using callback function:
+```
+exchangify.exchange(100, "EUR", "HRK", (error, amount) => {
+    if (error) return console.log(error)
+    console.log(amount)
+})
+```
+
+```
+amount: Number
+```
+
+### Dependencies
+None.
+
+## Contributing
+
+Please read [CONTRIBUTING.md](https://gist.github.com/PurpleBooth/b24679402957c63ec426) for details on our code of conduct, and the process for submitting pull requests to us.
+
+
+## Authors
+
+* **Dušan Barić**
+* **Pavle Andrić**
+
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
---
+++ index.js
@@ -0,0 +1,68 @@
+const https = require('https')
+
+class Exchangify {
+    constructor(appId) {
+        this.appId = appId
+        this.baseURL = "https://openexchangerates.org/api"
+        this.ratesResponse = null
+        this.refreshTime = 0
+    }
+
+    exchange(amount, from, to, cb) {
+        if (!this.appId) throw new Error("You forgot to set 'Openexchangerates app id' i class constructor")
+        return new Promise(async (resolve, reject) => {
+            try {
+                resolve(await this._getExchangeRates())
+            } catch (error) {
+                this.lastRefreshTime = 0
+                this.refreshTime = 0
+                if (cb) cb(error, null)
+                reject(error)
+            }
+        }).then(response => {
+            this.ratesResponse = response
+            const result = this._convert(amount, from, to, response.rates)
+            if (cb) cb(null, result)
+            return result
+        })
+    }
+
+    _convert(amount, from, to, rates) {
+        return (amount / rates[from]) * rates[to]
+    }
+
+    _getExchangeRates() {
+        return new Promise(async (resolve, reject) => {
+            if (this._shouldRefreshRates()) {
+                this.lastRefreshTime = Date.now()
+                this.refreshTime = this.lastRefreshTime + (5000)
+                await this._fetchRates((error, response) => {
+                    if (error) reject(error)
+                    resolve(response)
+                })
+            }
+        })
+    }
+
+    _shouldRefreshRates() {
+        return !this.ratesResponse || this.refreshTime < Date.now()
+    }
+
+    _fetchRates(cb) {
+        https.get(`${this.baseURL}/latest.json?app_id=${this.appId}`, (resp) => {
+            let data = ""
+            resp.on("data", (chunk) => {
+                data += chunk
+            })
+            resp.on("end", () => {
+                const response = JSON.parse(data)
+                cb(null, response)
+            })
+
+        }).on("error", (err) => {
+            cb(error, null)
+        })
+    }
+}
+
+module.exports = Exchangify
